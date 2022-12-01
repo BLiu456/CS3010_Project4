62f542637a3b6876353572ce156d6a9a1a93a52a
Nub456 <benjaminliu456@gmail.com>
2022-11-30 18:35:59 -0800

    Added lagrange and display newton's form

--- CS3010_Project4/CS3010_Project4.cpp
+++ CS3010_Project4/CS3010_Project4.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 //Function Prototypes here
 vector<vector<double>> getData();
+void lagrange(vector<vector<double>>);
 void newtonPoly(vector<vector<double>>);
 void printVect(vector<vector<double>>);
 
@@ -22,6 +23,8 @@ int main()
 {
     vector<vector<double>> data = getData();
     newtonPoly(data);
+    cout << endl;
+    lagrange(data);
 }
 
 vector<vector<double>> getData()
@@ -52,6 +55,57 @@ vector<vector<double>> getData()
     }
 
     return data;
+}
+
+void lagrange(vector<vector<double>> x)
+{
+    vector<double> d;
+    for (int i = 0; i < x.at(0).size(); i++)
+    {
+        double val = 1;
+        for (int j = 0; j < x.at(0).size(); j++)
+        {
+            if (i != j)
+            {
+                val *= x.at(0).at(i) - x.at(0).at(j);
+            }
+        }
+        d.push_back(val);
+    }
+    cout << "Lagrange polynomial:\n";
+    for (int i = 0; i < x.at(0).size(); i++)
+    {
+        double c = x.at(1).at(i) / d.at(i);
+        if (i != 0)
+        {
+            if (c < 0)
+            {
+                cout << " - ";
+            }
+            else if (c >= 0)
+            {
+                cout << " + ";
+            }
+        }
+        cout << abs(c);
+        for (int j = 0; j < x.at(0).size(); j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            cout << "(x";
+            if (x.at(0).at(j) < 0)
+            {
+                cout << " + ";
+            }
+            else if (x.at(0).at(j) >= 0)
+            {
+                cout << " - ";
+            }
+            cout << x.at(0).at(j) << ")";
+        }
+    }
 }
 
 void newtonPoly(vector<vector<double>> x)
@@ -98,7 +152,45 @@ void newtonPoly(vector<vector<double>> x)
     }
 
     //Creating the polynomial
-
+    vector<vector<double>> poly;
+    for (int i = 0; i < a.size(); i++)
+    {
+        vector<double> temp;
+        temp.push_back(a.at(i).at(0));
+        if (i != 0)
+        {
+            temp.push_back(x.at(0).at(i-1));
+        }
+        poly.push_back(temp);
+    }
+    //Displaying the polynomical
+    cout << "\nNetwon's interpolated polynomial:\n";
+    cout << poly.at(0).at(0);
+    for (int i = 1; i < poly.size(); i++)
+    {
+        if (poly.at(i).at(0) < 0)
+        {
+            cout << " - ";
+        }
+        else if (poly.at(i).at(0) >= 0)
+        {
+            cout << " + ";
+        }
+        cout << abs(poly.at(i).at(0));
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "(x";
+            if (poly.at(j).at(1) < 0)
+            {
+                cout << " + ";
+            }
+            else if (poly.at(j).at(1) >= 0)
+            {
+                cout << " - ";
+            }
+            cout << abs(poly.at(j).at(1)) << ")";
+        }
+    }
 }
 
 void printVect(vector<vector<double>> v)
